b547bf3b312ae427f29f0ac9e4ef02fd2d02e190
Sergio Soto Núñez <scots4ever@gmail.com>
2025-11-13 15:45:46 +0100

    feat: web application migrated to django

---
+++ maps/urls.py
@@ -0,0 +1,16 @@
+"""
+URLs de la aplicación maps
+"""
+from django.urls import path
+from . import views
+
+app_name = 'maps'
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('mapa/', views.mapa, name='mapa'),
+    path('api/trees/', views.get_trees, name='api_trees'),
+    path('api/stumps/', views.get_stumps, name='api_stumps'),
+]
+
+
